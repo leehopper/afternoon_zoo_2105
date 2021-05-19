3f99d8ca9a45a578e95665218107966ad64ef50f
Lee Hopper <hopper.lee.j@gmail.com>
2021-05-19 15:25:25 -0600

    Change age instance variable to age_weeks

--- lib/animal.rb
+++ lib/animal.rb
@@ -1,10 +1,10 @@
 class Animal
-  attr_reader :kind
+  attr_reader :kind, :age_weeks
 
   def initialize(kind, weight, age)
     @kind = kind
     @weight = weight
-    @age = age
+    @age_weeks = age
   end
 
   def weight
@@ -12,14 +12,14 @@ class Animal
   end
 
   def age
-    "#{@age} weeks"
+    "#{@age_weeks} weeks"
   end
 
   def age_in_days
-    @age * 7
+    @age_weeks * 7
   end
 
   def feed!(food)
     @weight += food
-  end 
+  end
 end
