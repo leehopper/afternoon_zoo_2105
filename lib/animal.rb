21d94fbe387f1a27fc27f8d7594211944c6fc86f
Lee Hopper <hopper.lee.j@gmail.com>
2021-05-19 14:18:52 -0600

    Change iniitialize method create age in days instance variable

--- lib/animal.rb
+++ lib/animal.rb
@@ -1,9 +1,10 @@
 class Animal
-  attr_reader :kind, :weight, :age
+  attr_reader :kind, :weight, :age, :age_in_days
 
   def initialize(kind, weight, age)
     @kind = kind
-    @weight = weight
-    @age = age
+    @weight = "#{weight} pounds"
+    @age = "#{age} weeks"
+    @age_in_days = age * 7
   end
 end
