49fbada8b10b70661605c6a5bc6e6597ed3c1dd1
Lee Hopper <hopper.lee.j@gmail.com>
2021-05-19 14:46:16 -0600

    Build tests to check if class object outputs the arguments

--- spec/zoo_spec.rb
+++ spec/zoo_spec.rb
@@ -3,8 +3,27 @@ require './lib/animal'
 require './lib/zoo'
 
 RSpec.describe Zoo do
-  it 'exists' do
-    zoo = Zoo.new("Bronx Zoo", "2300 Southern Blvd", "Bronx", "NY", "10460")
-
-    expect(zoo).to be_a(Zoo)
+  before(:each) do
+    @zoo = Zoo.new('Bronx Zoo', '2300 Southern Blvd', 'Bronx', 'NY', '10460')
   end
+
+  it 'exists' do
+    expect(@zoo).to be_a(Zoo)
+  end
+
+  it 'has a name' do
+    expect(@zoo.name).to eq('Bronx Zoo')
+  end
+
+  it 'has a street, city, state, and zip code' do
+    expect(@zoo.street).to eq('2300 Southern Blvd')
+    expect(@zoo.city).to eq('Bronx')
+    expect(@zoo.state).to eq('NY')
+    expect(@zoo.zip_code).to eq('10460')
+  end
+
+  xit 'has no inventory or animals to start' do
+    expect(@zoo.inventory).to eq([])
+    expect(@zoo.animal_count).to eq(0)
+  end
+end
